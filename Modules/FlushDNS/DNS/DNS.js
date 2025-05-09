efcdb8228efb92220efdf186b4c51a88be6fc363
uslsu <uszhayu@gmail.com>
2025-05-09 15:57:27 +0800

    Update DNS.js

--- Modules/FlushDNS/DNS/DNS.js
+++ Modules/FlushDNS/DNS/DNS.js
@@ -1,9 +1,29 @@
-let args = $argument.split('&').reduce((acc, curr) => {
-    let [key, value] = curr.split('=');
-    acc[key] = value;
-    return acc;
-}, {});
+/**
+ * Clear Surge DNS Cache (TF Compatible)
+ * - Clears DNS cache using HTTP API (/v1/dns/flush).
+ * - Updates panel with DNS delay information.
+ */
+!(async () => {
+    let panel = { title: "DNS Flush" };
+    if (typeof $argument != "undefined") {
+        let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+        if (arg.title) panel.title = arg.title;
+        if (arg.icon) panel.icon = arg.icon;
+        if (arg.color) panel["icon-color"] = arg.color;
+    }
+    if ($trigger == "button") {
+        await httpAPI("/v1/dns/flush");
+        $notification.post(panel.title, "", "DNS cache cleared successfully.");
+    }
+    let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0);
+    panel.content = `DNS延迟: ${delay}ms`;
+    $done(panel);
+})();
 
-$surge.clearDNSCache();
-$notification.post(args.title || "DNS Cache Cleared", "", "Surge DNS cache has been successfully cleared.");
-$done();
+function httpAPI(path = "", method = "POST", body = null) {
+    return new Promise((resolve) => {
+        $httpAPI(method, path, body, (result) => {
+            resolve(result);
+        });
+    });
+}
